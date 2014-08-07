b99759114f23eec61f63002b332e7f096c56fea2
Kalyan G <kalyan.g.subramanian@gmail.com>
2014-08-07 13:56:15 -0700

    fixed styling issues

--- cityontheset/tests/test_movies.py
+++ cityontheset/tests/test_movies.py
@@ -7,43 +7,43 @@ import unittest
 OBJECT_ENDPOINT = "movies"
 class Test_Movies(unittest.TestCase):
     def test_getById(self):
-        queryParams = {"id":1}
-        data = helpers.invokeURL(OBJECT_ENDPOINT, queryParams)
+        query_params = {"id":1}
+        data = helpers.invokeURL(OBJECT_ENDPOINT, query_params)
         self.assertEqual(data[0]['id'], 1)
 
     def test_getByTitle(self):
-        queryParams = {"name":"dawn of the planet"}
-        data = helpers.invokeURL(OBJECT_ENDPOINT, queryParams)
+        query_params = {"name":"dawn of the planet"}
+        data = helpers.invokeURL(OBJECT_ENDPOINT, query_params)
         self.assertEqual(len(data), 1)
 
     def test_getByTitle_Empty(self):
-        queryParams =  {"name":""}
-        data = helpers.invokeURL(OBJECT_ENDPOINT, queryParams)
+        query_params =  {"name":""}
+        data = helpers.invokeURL(OBJECT_ENDPOINT, query_params)
         self.assertEqual(len(data), len(helpers.invokeURL(OBJECT_ENDPOINT,None)))
 
     def test_getByTitle_NotExist(self):
-        queryParams =  {"name":"this movie does not exist"}
-        data = helpers.invokeURL(OBJECT_ENDPOINT, queryParams)
+        query_params =  {"name":"this movie does not exist"}
+        data = helpers.invokeURL(OBJECT_ENDPOINT, query_params)
         self.assertEqual(len(data), 0)
 
     def test_getByTitle_Multiple(self):
-        queryParams =  {"name":"the"}
-        data = helpers.invokeURL(OBJECT_ENDPOINT, queryParams)
+        query_params =  {"name":"the"}
+        data = helpers.invokeURL(OBJECT_ENDPOINT, query_params)
         self.assertEqual(len(data), 66)
 
     def test_getByTitle_Multiple_Limit(self):
-        queryParams =  {"name":"the", "limit":"10"}
-        data = helpers.invokeURL(OBJECT_ENDPOINT, queryParams)
+        query_params =  {"name":"the", "limit":"10"}
+        data = helpers.invokeURL(OBJECT_ENDPOINT, query_params)
         self.assertEqual(len(data), 10)
 
     def test_getByIdAndTitle(self):
-        queryParams = {"id":1, "name":"jitney"}
-        data = helpers.invokeURL(OBJECT_ENDPOINT, queryParams)
+        query_params = {"id":1, "name":"jitney"}
+        data = helpers.invokeURL(OBJECT_ENDPOINT, query_params)
         self.assertTrue(len(data) == 1 and data[0]['id'] == 1)
 
     def test_getByTitle_Sort_Desc(self):
-        queryParams = {"name":"the", "limit":10, "sort":"-release_year"}
-        data = helpers.invokeURL(OBJECT_ENDPOINT, queryParams)
+        query_params = {"name":"the", "limit":10, "sort":"-release_year"}
+        data = helpers.invokeURL(OBJECT_ENDPOINT, query_params)
         prev_release_yr = 9999
 
         for idx,elem in enumerate(data):
@@ -51,8 +51,8 @@ class Test_Movies(unittest.TestCase):
             prev_release_yr = data[idx]['release_year']
 
     def test_getByTitle_Sort_Asc(self):
-        queryParams = {"name":"the", "limit":10, "sort":"release_year"}
-        data = helpers.invokeURL(OBJECT_ENDPOINT, queryParams)
+        query_params = {"name":"the", "limit":10, "sort":"release_year"}
+        data = helpers.invokeURL(OBJECT_ENDPOINT, query_params)
         prev_release_yr = 0
 
         for idx,elem in enumerate(data):
@@ -60,9 +60,9 @@ class Test_Movies(unittest.TestCase):
             prev_release_yr = data[idx]['release_year']
 
     def test_SqlInjection(self):
-        queryParams = {"name":"ok;delete from movies where id = 1;"}
-        helpers.invokeURL(OBJECT_ENDPOINT, queryParams)
-        queryParams = {"id":"1"}
-        data = helpers.invokeURL(OBJECT_ENDPOINT, queryParams)
+        query_params = {"name":"ok;delete from movies where id = 1;"}
+        helpers.invokeURL(OBJECT_ENDPOINT, query_params)
+        query_params = {"id":"1"}
+        data = helpers.invokeURL(OBJECT_ENDPOINT, query_params)
         self.assertEqual(len(data), 1)
 
